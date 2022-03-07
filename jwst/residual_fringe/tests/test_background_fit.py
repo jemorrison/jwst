851e736a8e513bef03cac7c84ba3a34626abe94e
William Jamieson <wjamieson@stsci.edu>
2022-03-07 15:35:28 -0500

    Revert "Bugfix/res fringe background test"

--- jwst/residual_fringe/tests/test_background_fit.py
+++ jwst/residual_fringe/tests/test_background_fit.py
@@ -1,9 +1,6 @@
 """
 Unit test for Residual Fringe Correction fitting of the background
 """
-import pytest
-
-from pathlib import Path
 
 from jwst.residual_fringe import utils
 from numpy.testing import assert_allclose
@@ -16,24 +13,41 @@ def read_fit_column(file):
     # Data was pulled out of an exposure by modifying residual_fringe.py to write out a column of data
     # The function we are testing is fit_1d_background_complex.
 
-    file_dir = Path(__file__).parent.resolve()
-    file_path = str(file_dir / file)
-
-    hdu = fits.open(file_path)
+    hdu = fits.open(file)
     col_data = hdu[1].data
     col_weight = hdu[2].data
     col_wnum = hdu[3].data
     bg_fit = hdu[4].data
     store_freq = hdu[0].header['FFREQ']
-
-    bg_fit2, _ = utils.fit_1d_background_complex(col_data, col_weight,
-                                                 col_wnum, ffreq=store_freq)
-
+    results = (col_data, col_weight, col_wnum, bg_fit, store_freq)
+    return results 
 
 
-@pytest.mark.parametrize("file", ['good_col.fits', 'edge_col.fits'])
-def test_background_fit(file):
+
+def test_background_fit():
     """ test fit_1d_background_complex"""
 
+    # test a redefined good column
+    file1 = 'residual_col_633.fits'
+    results = read_fit_column(file1)
+    (col_data, col_weight, col_wnum, bg_fit, store_freq) = results 
+    
+    bg_fit2, bgindx2 = utils.fit_1d_background_complex(col_data, col_weight,
+                                                       col_wnum, ffreq=store_freq)
+    
 
-    read_fit_column(file)
+    assert_allclose(bg_fit, bg_fit2, atol=0.001)
+    
+
+    # test a redefined edge column (sometimes tricky to fit)
+    file2 = 'residual_col_433.fits'
+    read_fit_column(file2)
+
+    (col_data, col_weight, col_wnum, bg_fit, store_freq) = results 
+    
+    bg_fit2, bgindx2 = utils.fit_1d_background_complex(col_data, col_weight,
+                                                       col_wnum, ffreq=store_freq)
+    
+
+    assert_allclose(bg_fit, bg_fit2, atol=0.001)
+    
